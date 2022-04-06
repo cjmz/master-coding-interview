45405756ef153ad7f4c728be6ffbb68894140a5e
Claudio melo <claudiojm92@gmail.com>
2022-04-06 18:48:27 -0300

    add section 5 about arrays

---
+++ section-5-arrays/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	strings := []string{"a", "b", "c", "d"}
+	// 4x4 = 16 bytes of storage
+
+	fmt.Println(strings[2])
+
+	// append item in slice (push equivalent in JS)
+	strings = append(strings, "e") // O(1) = constant
+	fmt.Println(strings)
+
+	// remove the last item from slice (pop equivalent in JS)
+	strings = append(strings[:len(strings)-1], strings[len(strings):]...) // O(1) = constant
+	fmt.Println(strings)
+
+	// append item as first element in array (unshift equivalent in JS)
+	strings = append([]string{"X"}, strings...) // O(n) = constant
+	fmt.Println(strings)
+
+	// append item in specific position of an array
+	strings = append(strings[:3], strings[2:]...)
+	strings[2] = "O"
+	fmt.Println(strings)
+}
