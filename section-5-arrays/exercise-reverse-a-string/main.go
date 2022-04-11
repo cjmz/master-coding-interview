d705208d759725e81862c5f4340adef57f42ef5e
Claudio melo <claudiojm92@gmail.com>
2022-04-11 19:42:20 -0300

    more reusable

--- section-5-arrays/exercise-reverse-a-string/main.go
+++ section-5-arrays/exercise-reverse-a-string/main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 	const str = "Hi my name is Claudio"
+
+	fmt.Println(reverse(str))
+}
+
+func reverse(str string) string {
 	reverse := make([]byte, len(str))
 	for i := len(str) - 1; i >= 0; i-- {
 		reverse = append(reverse, str[i])
 	}
-	fmt.Println(string(reverse))
+	return string(reverse)
+
 }
