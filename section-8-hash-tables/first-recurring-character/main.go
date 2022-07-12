76bfb49c355874ba985f9c837109908da186bca5
Claudio melo <claudiojm92@gmail.com>
2022-07-12 18:26:33 -0300

    add section 8

---
+++ section-8-hash-tables/first-recurring-character/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{8, 12, 5, 1, 9, 12, 3, 4}
+	fmt.Println(solve(a))
+	fmt.Println(solve2(a))
+}
+
+// O(n^2)
+func solve(n []int) int {
+	// a := 2
+
+	for i := 0; i < len(n); i++ {
+		for j := i + 1; j < len(n); j++ {
+			if n[i] == n[j] {
+				return n[i]
+			}
+		}
+	}
+
+	return 0
+}
+
+// O(n)
+func solve2(n []int) int {
+	a := make(map[int]bool, len(n))
+
+	for i := 0; i < len(n); i++ {
+		if !a[n[i]] {
+			a[n[i]] = true
+			continue
+		}
+
+		return n[i]
+	}
+
+	return 0
+}
